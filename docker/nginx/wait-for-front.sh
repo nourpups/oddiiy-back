dd602479a0f1a81c9d4ddc4e879ace18ac10a6f6
anazhmiddinov <anazhmiddinov@team.amocrm.com>
2025-04-25 21:52:56 +0500

    final docker changes

--- docker/nginx/wait-for-front.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo "Waiting for front to be ready..."
-until curl -s http://front:3000 > /dev/null; do
-    echo "Front is not ready yet, retrying in 2 seconds..."
-    sleep 2
-done
-echo "Front is ready, starting NGINX..."
-exec nginx -g "daemon off;"
