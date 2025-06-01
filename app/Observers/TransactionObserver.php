be04669d820b0794d0e4f5c478213afcd54cfc40
anazhmiddinov <anazhmiddinov@team.amocrm.com>
2025-06-01 20:53:01 +0500

    fix order notifiction status changing

--- app/Observers/TransactionObserver.php
+++ app/Observers/TransactionObserver.php
@@ -5,6 +5,7 @@ namespace App\Observers;
 use App\Enum\OrderStatus;
 use App\Models\Order;
 use Goodoneuz\PayUz\Models\Transaction;
+use Illuminate\Support\Facades\Log;
 
 class TransactionObserver
 {
@@ -18,17 +19,40 @@ class TransactionObserver
 
     public function updated(Transaction $transaction): void
     {
+        Log::info('xoba', $transaction->toArray());
         // Проверяем, изменился ли статус транзакции
-        if ($transaction->isDirty('status') &&
-            $transaction->status !== Transaction::STATE_CREATED) {
-            $order = Order::find($transaction->transactionable_id);
+        if ($transaction->isDirty('state') &&
+            $transaction->state !== Transaction::STATE_CREATED) {
+            Log::info(sprintf(
+                "Транзакция с ID %d (%s) переходит в статус %d",
+                $transaction->id,
+                $transaction->payment_system,
+                $transaction->state,
+            ));
 
-            if ($order) {
-                if ($transaction->status === -1 || $transaction->status === -2) {
-                    $order->update(['status' => OrderStatus::CANCELLED]);
-                } elseif ($transaction->status === 2) {
-                    $order->update(['status' => OrderStatus::ACCEPTED]);
-                }
+            $order = Order::find($transaction->transactionable_id);
+            if (empty($order)) {
+                Log::error("Заказ с ID {$transaction->transactionable_id} не найден");
+                return;
+            }
+
+            if ($transaction->state === Transaction::STATE_CANCELLED ||
+                $transaction->state === Transaction::STATE_CANCELLED_AFTER_COMPLETE) {
+                $order->update(['status' => OrderStatus::CANCELLED]);
+                Log::info(sprintf(
+                    "Заказ с ID %d отменен (status - %d)",
+                    $order->id,
+                    OrderStatus::CANCELLED->value,
+                ));
+            }
+
+            if ($transaction->state === Transaction::STATE_COMPLETED) {
+                $order->update(['status' => OrderStatus::ACCEPTED]);
+                Log::info(sprintf(
+                    "Заказ с ID %d оплачен (status - %d)",
+                    $order->id,
+                    OrderStatus::ACCEPTED->value,
+                ));
             }
         }
     }
